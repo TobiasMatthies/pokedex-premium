a96a2bc8135dedbe51175a0a606cd481e82c7e0e
Tobias Matthies <tobiasmatthies@MacBook-Air-von-Tobias.local>
2022-08-25 13:40:50 -0400

    init function

--- script/detail.js
+++ script/detail.js
@@ -3,6 +3,12 @@ let image;
 let number = 3;
 
 
+async function initDetail() {
+    await loadPokemons();
+    renderPokemons();
+}
+
+
 async function loadPokemons() {
     let url = `https://pokeapi.co/api/v2/pokemon/3/`;
     let response = await fetch(url);
@@ -16,7 +22,7 @@ async function loadPokemons() {
  * 
  * this function renders pokemons, starting at the value of "start", until getting to the value of "end". The difference between those variables is always 20.
  */
-async function renderPokemons() {
+function renderPokemons() {
     document.getElementById('body').innerHTML += cardTemplate(number);
 }
 
