d29c3b7df12281c7627761325d9e14e1580f0df3
KevinWissmer <kevin.wissmer@mail.de>
2022-08-26 21:43:20 +0200

    searchbar added

--- script/allPokemonList.js
+++ script/allPokemonList.js
@@ -1,69 +1,132 @@
-let currentLoadedPokemon = 0;
+//let currentLoadedPokemon = 0;
+let totalMax = 898;
 let max = 898;
-let loadingMaximum = 20;
-let loading = false;
+let loadingMaximum = 50;
+//let loading = false;
 let allPokemon = [];
+let filteredPokemon = [];
+let pageNumber = 1;
+let maxPages = 18;
 
-let front= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png";
-let back= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/female/3.png";
 
-// async function getPokemon(index) {
-//     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
-//     let pkm = await response.json();
-//     return pkm;
-// }
-
-async function showAllPokemon(){
+async function initPokemonList(){
+    
     renderAllEmptyPokemonContainer();
-    await getAllPokemonList();
-    console.log(allPokemon);
+    await getAllPokemonList(); 
+    setMaxPages();
     renderAllPokemonContainerInnerContent();
 }
 
-async function loadAllPokemon(){
-    renderNextPokemons();
-    let allpkm = await getAllPokemonList();
-    allpkm.forEach((element, index) => {
-        allPokemon.push({
-            id: index + 1,
-            name: element.name,
-            img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
-        })
-    });
-    return allPokemon;
+function refreshPokemonList() {
+    renderAllEmptyPokemonContainer();
+    renderAllPokemonContainerInnerContent();
 }
 
+
+let inputId = "inputPageNumber";
+
+function goToPage(){
+    let currentNumber = parseInt(getInputValue());
+    if(currentNumber > maxPages){
+        currentNumber = maxPages;
+    }
+    if(currentNumber < 1){
+        currentNumber = 1;
+    }
+    pageNumber = currentNumber;
+    setInputValue(pageNumber);
+    refreshPokemonList();
+
+}
+
+function previousPage(){
+    if(pageNumber > 1){
+        pageNumber--;
+        setInputValue(pageNumber);
+        refreshPokemonList();
+    }
+}
+
+function nextPage(){
+    if(pageNumber < maxPages){
+        pageNumber++;
+        setInputValue(pageNumber);
+        refreshPokemonList();
+    }
+}
+
+function getInputValue() {
+    return document.getElementById("inputPageNumber").value;
+}
+
+function setInputValue(value) {
+    document.getElementById("inputPageNumber").value = value;
+}
+
+
+
+
 async function getAllPokemonList(){
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`;
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${totalMax}&offset=0`;
     let response = await fetch(url);
     let pkm = await response.json();
-    return pkm.results;
+    allPokemon = pkm.results;
+    filteredPokemon = allPokemon;
 }
 
 function renderAllPokemonContainerInnerContent() {
-    for (let index = loadingMaximum - 20; index <= loadingMaximum; index++) {
-        renderPokemonContainerInnerContent(index);
+    for (let index = loadingMaximum * (pageNumber - 1); index < loadingMaximum * (pageNumber) ; index++) {
+        if (index < max) {
+            renderPokemonContainerInnerContent(index);
+        }
     }
 }
 
 function renderAllEmptyPokemonContainer() {
     let contentContainer = document.getElementById("content");
-    for (let index = loadingMaximum - 20; index <= loadingMaximum; index++) {
-        contentContainer.innerHTML += templatePokemonContainer(index);
+    contentContainer.innerHTML = '';
+    for (let index = loadingMaximum * (pageNumber - 1); index < loadingMaximum * (pageNumber) ; index++) {
+        if (index < max) {
+            contentContainer.innerHTML += templatePokemonContainer(index);
+        }
     }
 }
 
 function renderPokemonContainerInnerContent(index) {
     let contentNameContainer = document.getElementById(`pkmNameBox_${index}`);
-    contentContainer.innerHTML = `${allPokemon[index].id}  ${allPokemon[index].name}`;
+    contentNameContainer.innerHTML = `${index + 1}  ${filteredPokemon[index].name}`;
     let contentImgContainer = document.getElementById(`pkmImgBox_${index}`);
-    contentContainer.innerHTML = templatePokemonContainerInnerContent(index);
+    contentImgContainer.innerHTML = templatePokemonContainerInnerContent(index);
 
 }
 
 
 function templatePokemonContainerInnerContent(index) {
-    return `<img src="${allPokemon[index]['sprites']['front_default']}" alt=""  >`;
+    return `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getIdFromUrl(filteredPokemon[index].url)}.png" alt=""  >`;
+}
+
+function getIdFromUrl(url){
+    return url.split("/")[6];
 }
 
 
+
+function filterPokemon(){
+    let searchvalue = document.getElementById('inputSearch').value;
+    let result = allPokemon.filter(pkm => pkm.name.includes(searchvalue) );
+    filteredPokemon = result;
+    resetPokemonList();
+}
+
+function resetPokemonList(){
+    setMaxPages();
+    max = filteredPokemon.length;
+    pageNumber = 1;
+    setInputValue(pageNumber);
+    refreshPokemonList();
+}
+
+function setMaxPages() {
+    maxPages = Math.ceil(filteredPokemon.length / loadingMaximum);
+    document.getElementById('inputPageNumber').max = maxPages;
+}
